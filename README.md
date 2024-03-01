de72716ebed3acd7f6a1fdf72c55182699a88afa
Rafael-Boscolo <rbf.boscolo@gmail.com>
2024-03-01 19:23:11 -0300

    desenvolvido responsividade

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Projeto Challenge Decodificado
 
+<p align="center" >
+  <img src="assets/challenge-thema-dark.png" alt="Demostração da nlw-spacetime" width="100%" />
+  <img src="assets/challenge-thema-light.png" alt="Demostração da nlw-spacetime" width="100%" />
+</p>
+
+
 ## Tecnologias Utilizadas
 - HTML
 - CSS
 - JavaScript
+
+## Sobre o Projeto:
+- Projeto responsivo apartie dos "max=width: 700px".
+- Possui thema Dark e salva o tema preferido do usuário.
+- Criptografa o código ou Descriptografa.
+
+## Link:
+[Acesse o projeto aqui!]()
--- js/app.js
+++ js/app.js
@@ -69,3 +69,32 @@ function ValidarCriptografia(a) {
 function Limpar() {
     let texto = document.querySelector('textarea').value = '';
 }
+
+// DARK THEME
+const changeThemeBtn = document.querySelector("#change-theme");
+
+function toggleDarkMode (){
+    document.body.classList.toggle("dark");
+}
+
+// Carregar o thema de preferencia
+function loadTheme () {
+    const darkMode = localStorage.getItem("dark");
+
+    if (darkMode){
+        toggleDarkMode();
+    }
+}
+
+loadTheme(); 
+
+changeThemeBtn.addEventListener("change", function() {
+    toggleDarkMode();    
+
+    // save
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark", 1)
+    }
+});
